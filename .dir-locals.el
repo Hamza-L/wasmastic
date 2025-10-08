c4bbfe1398fa472575949201b0ab9b229d8e05f1
Hamza-L <hlahmimsi@gmail.com>
2025-10-07 22:26:05 -0400

    added local environment variable dir-local file

---
+++ .dir-locals.el
@@ -0,0 +1,2 @@
+((nil . ((eval . (setenv "PATH" (concat "C:\\Users\\hlahm\\Projects\\wasmastic\\external\\emsdk;" (getenv "PATH"))))
+         (eval . (setenv "PATH" (concat "C:\\Users\\hlahm\\Projects\\wasmastic\\external\\emsdk\\upstream\\emscripten;" (getenv "PATH")))))))
